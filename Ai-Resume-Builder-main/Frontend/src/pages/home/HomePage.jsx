a297570c8d16176ba4552242030fbc075b0ae872
nitya0009 <nityanandini1804@gmail.com>
2026-08-15 12:27:36 +0530

    Remove Learn More button

--- Ai-Resume-Builder-main/Frontend/src/pages/home/HomePage.jsx
+++ Ai-Resume-Builder-main/Frontend/src/pages/home/HomePage.jsx
@@ -12,12 +12,7 @@ function HomePage() {
   const user = useSelector((state) => state.editUser.userData);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const handleClick = () => {
-    window.open(
-      "https://github.com/sahidrajaansari/Ai-Resume-Builder",
-      "_blank"
-    );
-  };
+ 
 
   useEffect(() => {
     const fetchResponse = async () => {
@@ -83,26 +78,7 @@ function HomePage() {
                   ></path>
                 </svg>
               </a>
-              <a
-                onClick={handleClick}
-                className="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg bg-gray-100 rounded-2xl sm:w-auto sm:mb-0 cursor-pointer"
-              >
-                Learn More
-                <svg
-                  className="w-4 h-4 ml-1"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-                  ></path>
-                </svg>
-              </a>
+              
             </div>
           </div>
           
